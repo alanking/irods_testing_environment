979b3f740814be4b81d50ab2995092e4b0e05443
Kory Draughn <korydraughn@ymail.com>
2026-01-10 14:36:33 -0500

    [ 259, 264, 270] Replace compose library usage
    And update requirements.txt for Python 3.12.
    
    Co-authored-by: Alan King <alanking@renci.org>

---
+++ compose/container.py
@@ -0,0 +1,8 @@
+"""Minimal container wrapper for compose project container listings."""
+
+
+class Container:
+    """Container wrapper exposing only attributes used by this project."""
+
+    def __init__(self, name):
+        self.name = name
